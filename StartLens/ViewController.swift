38bf6cced356b59febaef0d522a608932a48aa15
Yuta Nakano <nakano.yuta252@gmail.com>
2020-05-08 23:15:55 +0900

    Initial Commit

---
+++ StartLens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StartLens
+//
+//  Created by 中野　裕太 on 2020/05/08.
+//  Copyright © 2020 Nakano Yuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
